044f8f37abeb7294bab537d000c51ad661f30d95
Jantoom <75770311+Jantoom@users.noreply.github.com>
2025-04-03 19:30:03 +1000

    Added region radio buttons and figure callback to handle selection. Restyled app with new colors and layouts.

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
-from globals import FORMATTED_CSV_PATH
+from dash import Dash, dcc, html, Input, Output, callback
+from globals import FORMATTED_CSV_PATH, COLORS
 from data import format_data
 import plotly.express as px
 import pandas as pd
 import os
 
 app = Dash()
+formatted_data = None
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
@@ -18,18 +19,69 @@ else:
     formatted_data = format_data()
 
 
-fig_1_data = (
-    formatted_data.groupby("Date", as_index=False)["Sales"]
-    .sum()
-    .sort_values(by=["Date"])
-)
-
-fig_1 = px.line(fig_1_data, x="Date", y="Sales")
-
 app.layout = html.Div(
     [
-        html.H1("Soul Food Pink Morsel Sales"),
-        html.Span("Sales visualisation for Soul Food's Pink Morsel product."),
-        dcc.Graph(id="pink-morsel-sales-line-chart", figure=fig_1),
-    ]
+        html.Div(
+            [
+                html.H1(
+                    "Soul Food Pink Morsel Sales",
+                    style={"textAlign": "center", "color": COLORS["text"]},
+                ),
+                html.Div(
+                    "Sales visualisation for Soul Food's Pink Morsel product.",
+                    style={"textAlign": "center", "color": COLORS["text"]},
+                ),
+            ],
+        ),
+        html.Div(
+            children=[
+                html.H4("Region/s selected:", style={"paddingLeft": 30}),
+                dcc.RadioItems(
+                    id="fig-1-region-buttons",
+                    options=["All", "North", "South", "East", "West"],
+                    value="All",
+                    inline=True,
+                    style={"paddingLeft": 30},
+                ),
+                dcc.Graph(id="fig-1-line-chart"),
+            ],
+            style={
+                "display": "flex",
+                "flexDirection": "column",
+                "flexGrow": 1,
+                "justifyContent": "center",
+            },
+        ),
+    ],
+    style={
+        "backgroundColor": COLORS["background"],
+        "display": "flex",
+        "flexDirection": "column",
+        "height": "100vh",
+        "width": "100vw",
+    },
 )
+
+
+@callback(Output("fig-1-line-chart", "figure"), Input("fig-1-region-buttons", "value"))
+def update_figure_1(selected_region):
+    accepted_regions = (
+        [str.lower(selected_region)]
+        if selected_region != "All"
+        else ["north", "south", "east", "west"]
+    )
+    figure_1_data = (
+        formatted_data.loc[formatted_data["Region"].isin(accepted_regions)]
+        .groupby("Date", as_index=False)["Sales"]
+        .sum()
+        .sort_values(by=["Date"])
+    )
+
+    figure_1 = px.line(figure_1_data, x="Date", y="Sales")
+    figure_1.update_layout(
+        plot_bgcolor=COLORS["primary"],
+        paper_bgcolor=COLORS["background"],
+        font_color=COLORS["text"],
+    )
+
+    return figure_1
--- src/globals.py
+++ src/globals.py
@@ -1,6 +1,18 @@
+# Data
 RAW_CSV_PATHS = [
     "data/daily_sales_data_0.csv",
     "data/daily_sales_data_1.csv",
     "data/daily_sales_data_2.csv",
 ]
 FORMATTED_CSV_PATH = "data/out.csv"
+
+# Styling
+COLORS = {
+    "primary": "#2C3E50",     # Dark blue gray
+    "secondary": "#18BC9C",   # Teal
+    "accent": "#E74C3C",      # Soft red
+    "background": "#ECF0F1",  # Light gray
+    "text": "#2C3E50",        # Dark gray
+    "link": "#2980B9",        # Blue
+    "border": "BDC3C7"        # Light gray
+}
