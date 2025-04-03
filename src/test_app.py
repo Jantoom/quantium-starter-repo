5ab8b1497a35f127daf7329492356aad9c2db01c
Jantoom <75770311+Jantoom@users.noreply.github.com>
2025-04-03 23:54:57 +1000

    Simplified test file even further.

--- src/test_app.py
+++ src/test_app.py
@@ -2,20 +2,20 @@ from app import app
 from dash.testing.composite import DashComposite
 import pytest
 
+
 @pytest.fixture(scope="function")
 def dash_app(dash_duo: DashComposite):
     dash_duo.start_server(app)
     return dash_duo
 
+
 def test_header_present(dash_app: DashComposite):
-    dash_app.wait_for_element_by_id("header-1", timeout=10)
-    assert dash_app.find_element("#header-1")
+    dash_app.wait_for_element_by_id("header-1", timeout=4)
+
 
 def test_visualisation_present(dash_app: DashComposite):
-    dash_app.wait_for_element_by_id("fig-1-line-chart", timeout=10)
-    assert dash_app.find_element("#fig-1-line-chart")
+    dash_app.wait_for_element_by_id("fig-1-line-chart", timeout=4)
 
 
 def test_region_picker_present(dash_app: DashComposite):
-    dash_app.wait_for_element_by_id("fig-1-region-picker", timeout=10)
-    assert dash_app.find_element("#fig-1-region-picker")
+    dash_app.wait_for_element_by_id("fig-1-region-picker", timeout=4)
